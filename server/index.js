47c757c6e15605688679819a393208f3e6008c06
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-06-22 11:58:43 +0100

    added routes

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { register } from ".controllers/auth.js";
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
 
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url);
@@ -36,8 +37,10 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /* Routes with Files */
-app.post('/auth/register', upload.single("picture"), register);
+app.post("/auth/register", upload.single("picture"), register);
 
+/* Routes */
+app.use("/auth", authRoutes);
 
 /* Mongoose Setup */
 const PORT = process.env.PORT || 6001;
